29d827a78cac5a11404c850209c3080c5fa579d9
nicmorais <nmorais.st@gmail.com>
2022-10-08 15:30:06 -0300

    Troca informações do gráfico da Home por dados reais

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:calvin_boards/components/default_drawer.dart';
 import 'package:calvin_boards/models/sign_up.dart';
 import 'package:calvin_boards/providers/signup_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
@@ -18,6 +19,21 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  List<_Point> soyExports2021 = [
+    _Point('Jan', 49.606),
+    _Point('Fev', 2646.546),
+    _Point('Mar', 12694.341),
+    _Point('Abr', 16619.467),
+    _Point('Mai', 15465.736),
+    _Point('Jun', 11568.091),
+    _Point('Jul', 8675.830),
+    _Point('Ago', 6480.259),
+    _Point('Set', 4858.458),
+    _Point('Out', 3294.671),
+    _Point('Nov', 2605.951),
+    _Point('Dez', 2739.225),
+  ];
+
   @override
   Widget build(BuildContext context) {
     //SignUp signUp = ModalRoute.of(context)!.settings.arguments as SignUp;
@@ -26,41 +42,66 @@ class _HomePageState extends State<HomePage> {
         drawer: ChangeNotifierProvider<SignUpProvider>.value(
             builder: (context, child) => DefaultDrawer(),
             value: Provider.of<SignUpProvider>(context)),
-        body: _buildChart());
+        body: Column(children: [
+          Expanded(child: _buildChart()),
+        ]));
   }
 
   Widget _buildChart() {
     return SfCartesianChart(
         margin:
             const EdgeInsets.only(top: 60, left: 40, right: 40, bottom: 100),
+        primaryYAxis: NumericAxis(
+            title: AxisTitle(text: "Milhões de toneladas"),
+            numberFormat: NumberFormat.compact()),
         primaryXAxis: CategoryAxis(),
-        // Chart title
         title: ChartTitle(text: 'Soja exportada por mês'),
-        // Enable legend
-        legend: Legend(isVisible: true),
-        // Enable tooltip
+        legend: Legend(
+            isVisible: true,
+            title: LegendTitle(
+                alignment: ChartAlignment.center,
+                text:
+                    "Exportação de soja caiu 21% em 2022.\nFonte: Comex Stat - MDIC")),
         tooltipBehavior: TooltipBehavior(enable: true),
-        series: <ChartSeries<_Plantacao, String>>[
-          LineSeries<_Plantacao, String>(
-              dataSource: <_Plantacao>[
-                _Plantacao('Jan', 35000),
-                _Plantacao('Feb', 28000),
-                _Plantacao('Mar', 34000),
-                _Plantacao('Apr', 32000),
-                _Plantacao('May', 40000),
-                _Plantacao('Jun', 60000)
+        series: <ChartSeries<_Point, String>>[
+          LineSeries<_Point, String>(
+              color: Colors.blue,
+              markerSettings: const MarkerSettings(isVisible: true),
+              name: "2021",
+              dataSource: soyExports2021,
+              xValueMapper: (_Point value, _) => value.month,
+              yValueMapper: (_Point value, _) => value.amount.round(),
+              dataLabelSettings: const DataLabelSettings(isVisible: true)),
+          LineSeries<_Point, String>(
+              color: Colors.green,
+              markerSettings: const MarkerSettings(isVisible: true),
+              name: "2022",
+              dataSource: <_Point>[
+                _Point('Jan', 2451.828),
+                _Point('Fev', 6274.365),
+                _Point('Mar', 12232.570),
+                _Point('Abr', 11481.981),
+                _Point('Mai', 10657.844),
+                _Point('Jun', 10088.221),
+                _Point('Jul', 7561.542),
+                _Point('Ago', 6117.405),
+                _Point('Set', 4292.326),
               ],
-              xValueMapper: (_Plantacao soja, _) => soja.year,
-              yValueMapper: (_Plantacao soja, _) => soja.soja,
-              // Enable data label
-              dataLabelSettings: const DataLabelSettings(isVisible: true))
+              xValueMapper: (_Point value, _) => value.month,
+              yValueMapper: (_Point value, _) => value.amount.round()),
+          AreaSeries(
+              name: "Período de alta",
+              color: const Color.fromARGB(19, 244, 67, 54),
+              dataSource: soyExports2021.sublist(2, 6),
+              xValueMapper: (_Point value, _) => value.month,
+              yValueMapper: (_Point value, _) => value.amount)
         ]);
   }
 }
 
-class _Plantacao {
-  _Plantacao(this.year, this.soja);
+class _Point {
+  _Point(this.month, this.amount);
 
-  final String year;
-  final double soja;
+  final String month;
+  final num amount;
 }
